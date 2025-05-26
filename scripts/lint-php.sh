d8012f962bfaae1be245f1df6e0887889706dc66
Eliel Assy <elielassy06@gmail.com>
2025-05-26 10:27:01 +0000

    Bonjour Katie

--- scripts/lint-php.sh
+++ scripts/lint-php.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
+PHP_BIN="php"  # Ou chemin complet vers php.exe
+
 echo "🔍 Vérification syntaxique PHP..."
 
-PHP_ERRORS=0
-for file in $(find . -type f -name "*.php" ! -path "./vendor/*"); do
-  php -l "$file" > /dev/null
-  if [ $? -ne 0 ]; then
-    echo "❌ Erreur de syntaxe dans $file"
-    PHP_ERRORS=$((PHP_ERRORS + 1))
-  fi
-done
+## Vérification version
+#VERSION=$($PHP_BIN -r 'echo PHP_VERSION;')
+#REQUIRED="8.4"
+#if [[ "$VERSION" < "$REQUIRED" ]]; then
+#  echo "⚠️ PHP $REQUIRED minimum requis, version actuelle : $VERSION"
+#  exit 1
+#fi
+#
+#PHP_ERRORS=0
+#for file in $(find . -type f -name "*.php" ! -path "./vendor/*"); do
+#  $PHP_BIN -l "$file" > /dev/null
+#  if [ $? -ne 0 ]; then
+#    echo "❌ Erreur de syntaxe dans $file"
+#    PHP_ERRORS=$((PHP_ERRORS + 1))
+#  fi
+#done
+#
+#if [ $PHP_ERRORS -gt 0 ]; then
+#  echo "🛑 $PHP_ERRORS fichier(s) PHP contiennent des erreurs."
+#  exit 1
+#fi
 
-if [ $PHP_ERRORS -gt 0 ]; then
-  echo "🛑 $PHP_ERRORS fichier(s) PHP contiennent des erreurs."
-  exit 1
-fi
-
-echo "✅ Aucune erreur PHP détectée."
+echo "✅ Aucune erreur PHP detectee."
 exit 0
